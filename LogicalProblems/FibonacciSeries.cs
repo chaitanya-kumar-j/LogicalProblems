27df1fe70a28fd168a233eb3897ae2382aeb02ab
chaitanya4294 <chaitanya.4294@gmail.com>
2021-09-22 20:17:24 +0530

    [Chaitanya] Add : Added Fibonacci series Program

---
+++ LogicalProblems/FibonacciSeries.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LogicalProblems
+{
+    class FibonacciSeries
+    {
+        public void Print(int n)
+        {
+            // define variables
+            int firstNumber=0, secondNumber = 1, nextNumber;
+            Console.WriteLine($"First {n} numbers of fibinacci series are: ");
+            for (int i=1; i<=n; i++)
+            {
+                nextNumber = firstNumber + secondNumber;
+                Console.Write($"{firstNumber} ");
+                firstNumber = secondNumber;
+                secondNumber = nextNumber;
+            }
+        }
+    }
+}
---
+++ LogicalProblems/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace LogicalProblems
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 
+            bool isRun = true;
+            while (isRun)
+            {
+                // Select the Program number to run or to exit
+                Console.WriteLine("\nEnter any key otherthan number from 1 to 6, to exit program\n or");
+                Console.WriteLine("To run program, Select and enter Program number between 1 and 6: \n" +
+                    "(1. Fibonacci series, 2. Perfect number, 3. Prime number\n" +
+                    "4. Reverse a number, 5. Coupan number, 6. Stop watch)\n");
+                int programOption = Convert.ToInt32(Console.ReadLine());
+                switch (programOption)
+                {
+                    case 1:
+                        new FibonacciSeries().Print(10);
+                        break;
+                    default:
+                        isRun = !isRun;
+                        break;
+                }
+            }
+        }
+    }
+}
